18679440f8bd5bce3fffe1bdef1547a3fdb2bfa4
Carmen Ortigoza <43401320+ZzalyOreo@users.noreply.github.com>
2022-10-24 16:43:01 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+
+//AGREGA TUS ENLACES DE FIREBASE AQUÍ
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Inicia el código
+
+                  //Finaliza el código
+            });
+      });
+}
+getData();
