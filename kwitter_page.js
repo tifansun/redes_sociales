91dfffb92a8eaa1117d821eba720617969308005
Carmen Ortigoza <43401320+ZzalyOreo@users.noreply.github.com>
2023-03-13 11:45:49 -0600

    format document

--- kwitter_page.js
+++ kwitter_page.js
@@ -2,8 +2,10 @@
 
 function getData() {
       firebase.database().ref("/" + room_name).on('value', function (snapshot) {
-            document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
-                  childKey = childSnapshot.key; childData = childSnapshot.val(); if (childKey != "purpose") {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key; childData = childSnapshot.val();
+                  if (childKey != "purpose") {
                         firebase_message_id = childKey;
                         message_data = childData;
                         //Inicia código
